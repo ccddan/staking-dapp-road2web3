5aa955a54ffd0ec9f9f2865bf1d45d0615e43b0f
ccddan <ccdd.dev@gmail.com>
2022-06-25 23:04:36 -0500

    fix: rename vars/fns to meet scaffold eth interface requirements

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -15,9 +15,46 @@ contract Staker {
     uint256 public claimDeadline = block.timestamp + 240 seconds;
     uint256 public currentBlock = 0;
 
+    // Events
     event Stake(address indexed sender, uint256 amount);
     event Received(address, uint256);
     event Execute(address indexed sender, uint256 amount);
+
+    // Modifiers
+    /*
+  Checks if the withdrawal period been reached or not
+  */
+    modifier withdrawalDeadlineReached(bool requireReached) {
+        uint256 timeRemaining = withdrawalTimeLeft();
+        if (requireReached) {
+            require(timeRemaining == 0, "Withdrawal period is not reached yet");
+        } else {
+            require(timeRemaining > 0, "Withdrawal period has been reached");
+        }
+        _;
+    }
+
+    /*
+  Checks if the claim period has ended or not
+  */
+    modifier claimDeadlineReached(bool requireReached) {
+        uint256 timeRemaining = claimPeriodLeft();
+        if (requireReached) {
+            require(timeRemaining == 0, "Claim deadline is not reached yet");
+        } else {
+            require(timeRemaining > 0, "Claim deadline has been reached");
+        }
+        _;
+    }
+
+    /*
+  Requires that contract only be completed once!
+  */
+    modifier notCompleted() {
+        bool completed = exampleExternalContract.completed();
+        require(!completed, "Stake already completed!");
+        _;
+    }
 
     constructor(address exampleExternalContractAddress) {
         exampleExternalContract = ExampleExternalContract(
@@ -25,24 +62,7 @@ contract Staker {
         );
     }
 
-    function withdrawalTimeLeft()
-        public
-        view
-        returns (uint256 _withdrawalTimeLeft)
-    {
-        return
-            block.timestamp > withdrawalDeadline
-                ? 0
-                : withdrawalDeadline - block.timestamp;
-    }
-
-    function claimPeriodLeft() public view returns (uint256 _claimPeriodLeft) {
-        return
-            block.timestamp > claimDeadline
-                ? 0
-                : claimDeadline - block.timestamp;
-    }
-
+    // Stake function for a user to stake ETH in our contract
     function stake()
         public
         payable
@@ -54,56 +74,77 @@ contract Staker {
         emit Stake(msg.sender, msg.value);
     }
 
+    /*
+  Withdraw function for a user to remove their staked ETH inclusive
+  of both principle and any accured interest
+  */
     function withdraw()
         public
         withdrawalDeadlineReached(true)
         claimDeadlineReached(false)
         notCompleted
     {
-        require(balances[msg.sender] > 0, "You have no balance to withdraw");
-        uint256 balance = balances[msg.sender];
-        uint256 rewards = balance +
+        require(balances[msg.sender] > 0, "You have no balance to withdraw!");
+        uint256 individualBalance = balances[msg.sender];
+        uint256 indBalanceRewards = individualBalance +
             ((block.timestamp - depositTimestamps[msg.sender]) *
                 rewardRatePerBlock);
         balances[msg.sender] = 0;
 
-        (
-            bool sent, /*bytes memory data*/
-
-        ) = msg.sender.call{value: rewards}("");
-        require(sent, "Withdrawal failed, try again");
+        // Transfer all ETH via call! (not transfer) cc: https://solidity-by-example.org/sending-ether
+        (bool sent, bytes memory data) = msg.sender.call{
+            value: indBalanceRewards
+        }("");
+        require(sent, "RIP; withdrawal failed :( ");
     }
 
+    /*
+  Allows any user to repatriate "unproductive" funds that are left in the staking contract
+  past the defined withdrawal period
+  */
     function execute() public claimDeadlineReached(true) notCompleted {
         uint256 contractBalance = address(this).balance;
-        exampleExternalContract.complete{value: contractBalance}();
+        exampleExternalContract.complete{value: address(this).balance}();
     }
 
-    // Modifiers
-    modifier withdrawalDeadlineReached(bool requireReached) {
-        uint256 timeRemaining = withdrawalTimeLeft();
-        requireReached
-            ? require(
-                timeRemaining == 0,
-                "Withdrawal period is not reached yet"
-            )
-            : require(timeRemaining > 0, "Claim deadline has been reached");
-        _;
+    /*
+  READ-ONLY function to calculate time remaining before the minimum staking period has passed
+  */
+    function withdrawalTimeLeft()
+        public
+        view
+        returns (uint256 withdrawalTimeLeft)
+    {
+        if (block.timestamp >= withdrawalDeadline) {
+            return (0);
+        } else {
+            return (withdrawalDeadline - block.timestamp);
+        }
     }
 
-    modifier claimDeadlineReached(bool requireReached) {
-        uint256 timeRemaining = claimPeriodLeft();
-        requireReached
-            ? require(timeRemaining == 0, "Claim deadline is not reached yet")
-            : require(timeRemaining > 0, "Claim deadline has been reached");
-        _;
+    /*
+  READ-ONLY function to calculate time remaining before the minimum staking period has passed
+  */
+    function claimPeriodLeft() public view returns (uint256 claimPeriodLeft) {
+        if (block.timestamp >= claimDeadline) {
+            return (0);
+        } else {
+            return (claimDeadline - block.timestamp);
+        }
     }
 
-    modifier notCompleted() {
-        require(
-            !exampleExternalContract.completed(),
-            "Stake already completed"
-        );
-        _;
+    /*
+  Time to "kill-time" on our local testnet
+  */
+    function killTime() public {
+        currentBlock = block.timestamp;
+    }
+
+    /*
+  \Function for our smart contract to receive ETH
+  cc: https://docs.soliditylang.org/en/latest/contracts.html#receive-ether-function
+  */
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
     }
 }
