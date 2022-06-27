13b42bf479d6f0914b048c26f0682d1d465800f8
ccddan <ccdd.dev@gmail.com>
2022-06-26 21:01:31 -0500

    feat: allow owner to send balance to staker contract

--- packages/hardhat/contracts/ExampleExternalContract.sol
+++ packages/hardhat/contracts/ExampleExternalContract.sol
@@ -2,9 +2,11 @@
 pragma solidity 0.8.4;
 
 contract ExampleExternalContract {
+    address public owner = 0xfF91afCAaFb322451B32b7Fb0894a251A256DEFe;
     bool public completed;
 
     function withdrawToStakerContract(address stakerContract) public {
+        require(msg.sender == owner, "Forbidden");
         require(completed, "Contract is not completed");
         (
             bool sent, /* bytes memory data */
