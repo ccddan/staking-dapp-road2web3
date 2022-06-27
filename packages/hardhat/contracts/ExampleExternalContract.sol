3080a0343c760ad4b8ca56cf7de19731891df715
ccddan <ccdd.dev@gmail.com>
2022-06-26 20:39:17 -0500

    feat: refund balance to staker contract

--- packages/hardhat/contracts/ExampleExternalContract.sol
+++ packages/hardhat/contracts/ExampleExternalContract.sol
@@ -4,6 +4,18 @@ pragma solidity 0.8.4;
 contract ExampleExternalContract {
     bool public completed;
 
+    function withdrawToStakerContract(address stakerContract) public {
+        require(completed, "Contract is not completed");
+        (
+            bool sent, /* bytes memory data */
+
+        ) = stakerContract.call{value: (address(this).balance - 0.01 ether)}(
+                ""
+            );
+        require(sent, "Probably not enough balance to pay for gas");
+        completed = false;
+    }
+
     function complete(bool value) public payable {
         completed = value;
     }
